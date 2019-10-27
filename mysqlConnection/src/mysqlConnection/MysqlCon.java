68fd3064533763cf80c0b8bdee9fa56365a81575
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-10-27 19:07:43 -0300

    Java to Mysql Connection

--- mysqlConnection/src/mysqlConnection/MysqlCon.java
+++ mysqlConnection/src/mysqlConnection/MysqlCon.java
@@ -8,13 +8,15 @@ public class MysqlCon {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world","root","root");
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from city;");
+			ResultSet rs = stmt.executeQuery("select * from city where CountryCode = 'AFG'"); //La misma query
 			while(rs.next()) {
 				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)
-				+" "+rs.getString(4)+" "+rs.getString(5));
-				con.close();
+				+" "+rs.getString(4)+" "+rs.getInt(5));
+				
 			}}
 			catch(Exception e){System.out.println(e);}
 		}
 	
 }
+
+ 
